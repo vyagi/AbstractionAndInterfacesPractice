2d353ebfd061bdb374077916a6e300e8191a1be5
Małgorzata Jagieła <malgorzata.jagiela@hotmail.com>
2024-12-16 18:56:31 +0100

    Add project files.

---
+++ Stationery/Stationery.cs
@@ -0,0 +1,47 @@
+﻿namespace Stationery;
+
+public class BallPen
+{
+    public void Write(string text) { }
+}
+
+public class Pencil
+{
+    public void Write(string text) { }
+}
+
+public class Crayon
+{
+    public void Write(string text) { }
+}
+
+public class StationeryTester
+{
+    public static void Write(string text, IEnumerable<object> instruments)
+    {
+        foreach (var instrument in instruments)
+        {
+            if (instrument is BallPen)
+            {
+                if (((BallPen)instrument).CanWrite)
+                {
+                    ((BallPen)instrument).Write(text);
+                }
+            }
+            else if (instrument is Pencil)
+            {
+                if (((Pencil)instrument).CanWrite)
+                {
+                    ((Pencil)instrument).Write(text);
+                }
+            }
+            else if (instrument is Crayon)
+            {
+                if (((Crayon)instrument).CanWrite)
+                {
+                    ((Pencil)instrument).Write(text);
+                }
+            }
+        }
+    }
+}
---
+++ UserInterface/Program.cs
@@ -0,0 +1,22 @@
+﻿using Stationery;
+
+//This file needs to compile and work. To achieve this you need to implement necessary changes in the Stationery.cs file in the Utilities project
+
+var redCrayon = new Crayon(ConsoleColor.Red);
+
+Console.WriteLine($"The red crayon is {(redCrayon.IsSharp ? "" : "not ")}sharp");
+redCrayon.Sharpen();
+Console.WriteLine($"The red crayon is {(redCrayon.IsSharp ? "" : "not ")}sharp");
+
+var blueCrayon = new Crayon(ConsoleColor.Blue);
+blueCrayon.Sharpen();
+
+var pencil = new Pencil();
+pencil.Sharpen();
+
+var ballPen = new BallPen();
+ballPen.Click();
+
+var writableInstruments = new WritingInstrument[] { redCrayon, blueCrayon, pencil, ballPen };
+
+StationeryTester.Write("Some text", writableInstruments);
